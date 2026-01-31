def4172eea70e66a59b07525ba693f0848e72733
ferbarajas11930 <fernando.barajas8229@alumnos.udg.mx>
2026-01-30 20:03:45 -0600

    nuevo

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/*let a : number = 5
+let b : number = 12
+
+let result = a + b
+
+console.log(`El resultado es: ${result}`)*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const suma = (a, b) => {
+    return a + b;
+};
+const result = suma(4, 6);
+console.log(`El resultado de la suma es: ${result}`);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/*let a : number = 5
+let b : number = 12
+
+let result = a + b
+
+console.log(`El resultado es: ${result}`)*/
+
+const suma = (a: number, b: number): number => {return a + b}
+
+    const result = suma(4,6)
+    console.log(`El resultado de la suma es: ${result}`)
